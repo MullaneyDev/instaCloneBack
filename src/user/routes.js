fec19f43073974b5e5756f3bef6eef79c340febf
Nathan Stringer <nathstringer@gmail.com>
2023-10-31 16:11:28 +0000

    slight error in the users/routes fixed

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-
 const {
   registerUser,
   loginUser,
@@ -9,9 +8,7 @@ const {
   photosByUser,
 } = require("./controllers");
 
-const { registerUser, loginUser, getAllUsers } = require("./controllers");
 const { hashPass, comparePass, tokenCheck } = require("../middleware");
-
 
 // register user in the body
 userRouter.post("/register", hashPass, registerUser);
@@ -22,12 +19,10 @@ userRouter.post("/login", comparePass, loginUser);
 // get all users, mainly used for testing on backend
 userRouter.get("/", getAllUsers);
 
-
 // get pictures of username
 userRouter.get("/:username", photosByUser);
 
 // token check for persistent login
 userRouter.get("/authCheck", tokenCheck, loginUser);
-
 
 module.exports = userRouter;
