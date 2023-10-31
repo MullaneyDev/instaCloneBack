92ac462735cc2df77395a06e31a9d01f3666aa1d
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-31 14:43:51 +0000

    addition: middleware/index.js joe

---
+++ src/middleware/index.js
@@ -0,0 +1,64 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const saltRounds = parseInt(process.env.SALT_ROUNDS);
+
+const User = require("../user/model");
+
+const hashPass = async (res, req, next) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, saltRounds);
+    next();
+  } catch (error) {
+    res.status(501).json({ errormessage: error.message, error });
+  }
+};
+
+const comparePass = async (res, req, next) => {
+  try {
+    if (!req.body.username) {
+      res.status(500).json({ message: "Username cannot be blank." });
+      return;
+    }
+
+    req.user = await User.findOne({ where: { username: req.body.username } });
+    if (!req.user) {
+      res.status(401).json({ errormessage: "Invalid username." });
+      return;
+    }
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      req.user.password
+    );
+    if (!passwordMatch) {
+      res.status(401).json({ message: "Unauthorised Login!" });
+      return;
+    }
+    next();
+  } catch (error) {
+    res.status(501).json({ errormessage: error.message, error });
+  }
+};
+
+const tokenCheck = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decodedToken = await jwt.verify(token, process.env.SECRET_KEY);
+    req.user = await User.findOne({ where: { id: decodedToken.id } });
+    if (!req.user) {
+      const error = new Error("User is not Authorised");
+      res.status(401).json({ errorMessage: error.message, error: error });
+    }
+
+    req.passwordMatch = true;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({ errorMessage: error.message, error: error });
+  }
+};
+
+module.exports = {
+  hashPass,
+  comparePass,
+  tokenCheck,
+};
