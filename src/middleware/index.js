944fd79658e61af9fb98531a103d816a1eb68815
MullaneyDev <matt.mullaney@live.co.uk>
2023-10-31 17:30:56 +0000

    fix of middleware error messages to allow connection to front end

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -9,7 +9,7 @@ const hashPass = async (req, res, next) => {
     req.body.password = await bcrypt.hash(req.body.password, saltRounds);
     next();
   } catch (error) {
-    res.status(501).json({ errormessage: error.message, error });
+    res.status(501).json({ message: error.message, error });
   }
 };
 
@@ -22,20 +22,21 @@ const comparePass = async (req, res, next) => {
 
     req.user = await User.findOne({ where: { username: req.body.username } });
     if (!req.user) {
-      res.status(401).json({ errormessage: "Invalid username." });
+      res.status(401).json({ message: "Invalid username." });
       return;
     }
-    const passwordMatch = await bcrypt.compare(
+    req.passwordMatch = await bcrypt.compare(
       req.body.password,
       req.user.password
     );
-    if (!passwordMatch) {
+    
+    if (!req.passwordMatch) {
       res.status(401).json({ message: "Unauthorised Login!" });
       return;
     }
     next();
   } catch (error) {
-    res.status(501).json({ errormessage: error.message, error });
+    res.status(501).json({ message: error.message, error });
   }
 };
 
@@ -46,14 +47,14 @@ const tokenCheck = async (req, res, next) => {
     req.user = await User.findOne({ where: { id: decodedToken.id } });
     if (!req.user) {
       const error = new Error("User is not Authorised");
-      res.status(401).json({ errorMessage: error.message, error: error });
+      res.status(401).json({ message: error.message, error: error });
     }
 
     req.passwordMatch = true;
     next();
   } catch (error) {
     console.log(error);
-    res.status(501).json({ errorMessage: error.message, error: error });
+    res.status(501).json({ message: error.message, error: error });
   }
 };
 
