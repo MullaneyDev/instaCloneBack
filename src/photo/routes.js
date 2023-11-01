23dc3de406dd7b79990f665c4b3b6896b63ce11a
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-11-01 09:01:21 +0000

    Delete photo functionality working with token check

--- src/photo/routes.js
+++ src/photo/routes.js
@@ -4,10 +4,11 @@ const photoRouter = Router();
 const Photo = require("./model");
 
 const { addPhoto, deletePhoto, getAllPhotos } = require("./controllers");
+const { tokenCheck } = require("../middleware");
 
 photoRouter.post("/", addPhoto);
 
-photoRouter.delete("/", deletePhoto);
+photoRouter.delete("/", tokenCheck, deletePhoto);
 
 photoRouter.get("/", getAllPhotos);
 
